450e35932bcfc33b70f9b6e34bbbabb03f2a6524
Mary <mary08101961@gmail.com>
2024-06-20 10:57:26 +0300

    z33

---
+++ z3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 10, 3, 8, 25, 5, 0 };
+        ReverseArray(array, array.Length - 1);
+    }
+    static void ReverseArray(int[] arr, int ind)
+    {
+        if (ind < 0)
+        {
+            return;
+        }
+        Console.Writeline(arr[ind]);
+        ReverseArray(arr, ind - 1);
+    }
+}
